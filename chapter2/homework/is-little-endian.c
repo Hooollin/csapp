dc446807146d75e0f8925b75362ea9a430abd5cb
hollin <lan-cin@qq.com>
2020-03-22 22:30:29 +0800

    optimized

--- chapter2/homework/is-little-endian.c
+++ chapter2/homework/is-little-endian.c
@@ -2,12 +2,8 @@
 #include <stdbool.h>
 
 bool is_little_endian(){
-    short bytes = 0x00ff;
-    unsigned char *byte = (unsigned char *) &bytes;
-    if(*byte > *(byte + 1)){
-        return 1;
-    }else
-        return 0;
+    int a = 1;
+    return *(char*)&a;
 }
 int main(){
     printf("%d", is_little_endian());
