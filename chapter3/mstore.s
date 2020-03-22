21c49fad1fbddc09231bdcc977f7f4f9cbe7a25a
hollin <lan-cin@qq.com>
2020-03-22 19:36:18 +0800

    first sample of chapter3

---
+++ chapter3/mstore.s
@@ -0,0 +1,32 @@
+	.file	"mstore.c"
+	.text
+	.globl	mult2
+	.type	mult2, @function
+mult2:
+.LFB23:
+	.cfi_startproc
+	movq	%rdi, %rax
+	imulq	%rsi, %rax
+	ret
+	.cfi_endproc
+.LFE23:
+	.size	mult2, .-mult2
+	.globl	multstore
+	.type	multstore, @function
+multstore:
+.LFB24:
+	.cfi_startproc
+	pushq	%rbx
+	.cfi_def_cfa_offset 16
+	.cfi_offset 3, -16
+	movq	%rdx, %rbx
+	call	mult2
+	movq	%rax, (%rbx)
+	popq	%rbx
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE24:
+	.size	multstore, .-multstore
+	.ident	"GCC: (Ubuntu 7.3.0-27ubuntu1~18.04) 7.3.0"
+	.section	.note.GNU-stack,"",@progbits
