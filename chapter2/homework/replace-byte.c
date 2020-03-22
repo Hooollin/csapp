736845a7d4bbfaf74ab3e712f5d231ba609bc769
hollin <lan-cin@qq.com>
2020-03-22 22:57:23 +0800

    optimized

--- chapter2/homework/replace-byte.c
+++ chapter2/homework/replace-byte.c
@@ -3,16 +3,14 @@
 typedef char byte;
 
 unsigned replace_byte(unsigned x, int i, unsigned char b){
-    if(i >= sizeof(x)){
+    if(i > sizeof(unsigned)){
         return x;
     }
-    byte *p = (byte*)&x;
-    *(p + i) = b;
-    return x;
+    return x & ~(0xFF << (i << 3)) | (b << (i << 3));
 }
 
 int main(){
     unsigned x = 0x12345678;
-    printf("0x%X",replace_byte(x, 0, 0xAB));
+    printf("0x%X",replace_byte(x, 2, 0xBB));
     return 0;
 }
