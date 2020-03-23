a8a443308c5d997bd7b4dad7fa39f08fd329044e
hollin <lan-cin@qq.com>
2020-03-23 12:56:41 +0800

    leftmost one

--- chapter2/homework/homework-2-66.c
+++ chapter2/homework/homework-2-66.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int leftmost_one(unsigned x){
-	x = x ^ (x >> 16);
-	x = x ^ (x >> 8);
-	x = x ^ (x >> 4);
-	x = x ^ (x >> 2);
-	x = x ^ (x >> 1);
-	return x & 1;
+    x |= x >> 16;
+    x |= x >> 8;
+    x |= x >> 4;
+    x |= x >> 2;
+    x |= x >> 1;
+    return x ^ (x >> 1);
 }
 
 int main(){
-    int x = 0x1111111;
-    printf("%d", leftmost_one(x));
+    int x = 0xFFFFFFFF;
+    printf("0x%X", leftmost_one(x));
 }
 
