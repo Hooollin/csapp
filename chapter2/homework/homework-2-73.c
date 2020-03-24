78eab05cbd820587eb6a5375bccff7eb5faf888b
hollin <lan-cin@qq.com>
2020-03-24 19:39:59 +0800

    optimized

--- chapter2/homework/homework-2-73.c
+++ chapter2/homework/homework-2-73.c
@@ -3,19 +3,17 @@
 #define TMin -2147483647 - 1
 
 int saturating_add(int x, int y){
-    if(x > 0 && y > 0 && x + y < x && x + y < y){
-        return TMax;
-    }
-    if(x < 0 && y < 0 && x + y > x && x + y > y){
-        return TMin;
-    }
-    return x + y;
+    int sum       = x + y;
+    int sign_mask = 1 << 31;
+    int pos_over  = !(x & sign_mask) && !(y & sign_mask) && (sum & sign_mask);
+    int neg_over  = (x & sign_mask) && (y & sign_mask) && !(sum & sign_mask);
+    (pos_over && (sum = TMax) || neg_over && (sum = TMin));
+    return sum;
 }
 
 int main(){
     int x = 0x80000000;
     int y = 0x80000000;
-
     printf("%d, %d, %d, %d", x, y, x + y, saturating_add(x, y));
     return 0;
 }
