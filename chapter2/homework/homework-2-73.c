8eb7f4bff366803f59a91f5d10442ef1fcb36d1f
hollin <lan-cin@qq.com>
2020-03-24 19:45:22 +0800

    prettify code

--- chapter2/homework/homework-2-73.c
+++ chapter2/homework/homework-2-73.c
@@ -6,7 +6,7 @@ int saturating_add(int x, int y){
     int sum       = x + y;
     int sign_mask = 1 << 31;
     int pos_over  = !(x & sign_mask) && !(y & sign_mask) && (sum & sign_mask);
-    int neg_over  = (x & sign_mask) && (y & sign_mask) && !(sum & sign_mask);
+    int neg_over  =  (x & sign_mask) &&  (y & sign_mask) && !(sum & sign_mask);
     (pos_over && (sum = TMax) || neg_over && (sum = TMin));
     return sum;
 }
