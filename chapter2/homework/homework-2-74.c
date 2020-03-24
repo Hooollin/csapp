ef713aff51a2465fb48ca835dedb57d5579d8a66
hollin <lan-cin@qq.com>
2020-03-24 19:53:59 +0800

    optimized

--- chapter2/homework/homework-2-74.c
+++ chapter2/homework/homework-2-74.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
 
 int t_sub_ok(int x, int y){
+    int sum       = x - y;
+    int sign_mask = 1 << 31;
     /*同号一定成功*/
-    if(x >= 0 && y >= 0 || x <= 0 && y <= 0){
-        return 1;
-    }
-    if(x > 0 && y < 0){
-        if(x - y < x && x - y < y){
-            return 0;
-        }
-    }
-    if(x < 0 && y > 0){
-        if(x - y > x && x - y > y){
-            return 0;
-        }
-    }
-    return 1;
+    int same_sign = (x & sign_mask) && (y & sign_mask) || !(x & sign_mask) && !(y & sign_mask);
+
+    int pos_over = !(x & sign_mask) && (y & sign_mask) && (sum & sign_mask);
+    int neg_over = (x & sign_mask) && !(y & sign_mask) && !(sum & sign_mask);
+    return same_sign || (!pos_over && !neg_over);
 }
 
 int main(){
-    int x = 0x7fffffff;
-    int y = 0xffffffff;
+    int x = 0x80000000;
+    int y = 0x01231232;
     printf("%d", t_sub_ok(x, y));
 }
 
