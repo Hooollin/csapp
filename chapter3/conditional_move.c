3f84213cfaea9b2e6b0926f4b7df7407d18b48e6
hollin <lan-cin@qq.com>
2020-03-27 14:59:32 +0800

    differences between almost same expressions

--- chapter3/conditional_move.c
+++ chapter3/conditional_move.c
@@ -1,3 +1,8 @@
 int cmovdiff(int x, int y){
     return x > y ? x : y;
 }
+
+
+int cread(long *xp){
+    return (xp ? *xp : 0);
+}
--- chapter3/conditional_move.s
+++ chapter3/conditional_move.s
@@ -12,5 +12,20 @@ cmovdiff:
 	.cfi_endproc
 .LFE0:
 	.size	cmovdiff, .-cmovdiff
+	.globl	cread
+	.type	cread, @function
+cread:
+.LFB1:
+	.cfi_startproc
+	testq	%rdi, %rdi
+	je	.L4
+	movl	(%rdi), %eax
+	ret
+.L4:
+	movl	$0, %eax
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	cread, .-cread
 	.ident	"GCC: (Ubuntu 7.3.0-27ubuntu1~18.04) 7.3.0"
 	.section	.note.GNU-stack,"",@progbits
